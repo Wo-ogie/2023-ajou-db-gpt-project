64a08a2aa7bd572da5ecdbdc6458fcae8e548d1f
Wo-ogie <siwall0105@gmail.com>
2023-05-29 01:51:10 +0900

    project settings

---
+++ backend/src/main/java/ajou/db/gpt/GptApplication.java
@@ -0,0 +1,13 @@
+package ajou.db.gpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GptApplication.class, args);
+    }
+
+}
